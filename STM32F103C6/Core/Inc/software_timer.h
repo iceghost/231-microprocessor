249a206ff89fa9b943d8ba93d97009d1203e8952
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-10-05 11:02:02 +0700

    software_timer: add helper handler struct

--- STM32F103C6/Core/Inc/software_timer.h
+++ STM32F103C6/Core/Inc/software_timer.h
@@ -7,6 +7,11 @@
 #define SOFTWARE_TIMERS_COUNT 0 
 #endif
 
+typedef struct {
+  size_t index;
+  uint32_t interval;
+} software_timer_handle_t;
+
 extern uint32_t software_timer_counts[SOFTWARE_TIMERS_COUNT];
 extern bool software_timer_flags[SOFTWARE_TIMERS_COUNT];
 
@@ -14,5 +19,11 @@ inline void software_timer_reset(size_t i, uint32_t count) {
   software_timer_counts[i] = count;
   software_timer_flags[i] = false;
 }
+
+inline void software_timer_item_reset(software_timer_handle_t *ti) {
+  software_timer_reset(ti->index, ti->interval);
+}
+#define IF_SOFTWARE_TIMER_ITEM_FLAGGED(ti)                                     \
+  for (; software_timer_flags[(ti)->index]; software_timer_item_reset(ti))
 
 #endif
