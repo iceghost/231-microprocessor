4a6f4e1c8003e0191105f549fc98996c0fdbd4c9
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-10-06 14:17:49 +0700

    matrix_display: add animation

--- Lib/Inc/matrix_display.h
+++ Lib/Inc/matrix_display.h
@@ -12,8 +12,16 @@ typedef struct {
   uint16_t row_pins[8];
   uint8_t active_en_i;
   software_timer_handle_t ti;
+  software_timer_handle_t animate_ti;
 } matrix_display_t;
 
+typedef struct {
+  size_t i;
+  uint8_t lshift;
+} matrix_display_state_t;
+
 void matrix_display_show(matrix_display_t *md, uint8_t *matrix);
+void matrix_display_animate(matrix_display_t *md, const char *str, size_t size,
+                            matrix_display_state_t *state);
 
 #endif
--- Lib/Src/matrix_display.c
+++ Lib/Src/matrix_display.c
@@ -1,4 +1,5 @@
 #include "matrix_display.h"
+#include "font8x8_basic.h"
 
 void matrix_display_show(matrix_display_t *md, uint8_t *matrix) {
   uint16_t on_en = md->en_pins[md->active_en_i];
@@ -23,5 +24,31 @@ void matrix_display_show(matrix_display_t *md, uint8_t *matrix) {
   IF_SOFTWARE_TIMER_ITEM_FLAGGED(&md->ti) {
     md->active_en_i += 1;
     md->active_en_i %= 8;
+  }
+}
+
+void matrix_display_animate(matrix_display_t *md, const char *str, size_t size,
+                            matrix_display_state_t *state) {
+  uint8_t matrix[8];
+
+  if (state->i >= size)
+    return;
+
+  uint8_t *curr = font8x8_basic[str[state->i]];
+  uint8_t *next = font8x8_basic[state->i < size - 1 ? str[state->i + 1] : ' '];
+
+  for (size_t i = 0; i < 8; i++) {
+    uint16_t buf = (next[i] << 8) | curr[i];
+    matrix[i] = (buf >> state->lshift) & 0xFF;
+  }
+
+  matrix_display_show(md, matrix);
+
+  IF_SOFTWARE_TIMER_ITEM_FLAGGED(&md->animate_ti) {
+    state->lshift += 1;
+    if (state->lshift == 8) {
+      state->lshift = 0;
+      state->i += 1;
+    }
   }
 }
