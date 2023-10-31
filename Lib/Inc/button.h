7b774ea80eba0043a3a11fb6675ab1e937976244
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-10-31 15:50:48 +0700

    lib: button: initialize own api

--- Lib/Inc/button.h
+++ Lib/Inc/button.h
@@ -1,20 +1,35 @@
-/*
- * button.h
- *
- *  Created on: Oct 5, 2023
- *      Author: KAI
- */
+#ifndef __BUTTON_H__
+#define __BUTTON_H__
 
-#ifndef INC_BUTTON_H_
-#define INC_BUTTON_H_
+#include "software_timer.h"
+#include "stm32f1xx_hal.h" // IWYU pragma: keep
+#include <stdbool.h>
 
-#include "main.h"
+typedef struct {
+  bool known;
+  bool current;
+  uint32_t stable_tick;
+  uint8_t debounce_threshold;
 
-#define NORMAL_STATE SET
-#define PRESSED_STATE RESET
+  GPIO_TypeDef *port;
+  uint16_t pin;
+} button_t;
 
-extern int button1_flag;
+inline void button_init(button_t *b, GPIO_TypeDef *port, uint16_t pin, uint8_t debounce_threshold) {
+  b->port = port;
+  b->pin = pin;
+  b->debounce_threshold = debounce_threshold;
+}
 
-void getKeyInput();
+void button_tick(button_t *b);
 
-#endif /* INC_BUTTON_H_ */
+inline bool button_is_pressed(button_t *b) {
+  return b->known == GPIO_PIN_RESET;
+}
+
+inline bool button_is_longpressed(button_t *b, uint32_t duration) {
+  return button_is_pressed(b) &&
+         software_timer_tick - b->stable_tick >= duration;
+}
+
+#endif
