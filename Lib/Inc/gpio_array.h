b75d6c4dbacade153aedeac2e0615988835cd844
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-11-15 22:02:30 +0700

    gpio_array: update

--- Lib/Inc/gpio_array.h
+++ Lib/Inc/gpio_array.h
@@ -14,18 +14,8 @@ typedef struct {
 
 void gpio_array_init(gpio_array_t *ga, GPIO_TypeDef *port, uint16_t *pins,
                      size_t len);
-
-inline void gpio_array_set(gpio_array_t *ga, uint16_t gpios) {
-  HAL_GPIO_WritePin(ga->port, gpios, GPIO_PIN_SET);
-}
-
-inline void gpio_array_reset(gpio_array_t *ga, uint16_t gpios) {
-  HAL_GPIO_WritePin(ga->port, gpios, GPIO_PIN_RESET);
-}
-
-inline void gpio_array_write(gpio_array_t *ga, uint16_t gpios) {
-  gpio_array_reset(ga, gpios);
-  gpio_array_set(ga, ga->mask & ~gpios);
-}
+void gpio_array_set(gpio_array_t *ga, uint16_t gpios);
+void gpio_array_reset(gpio_array_t *ga, uint16_t gpios);
+void gpio_array_write(gpio_array_t *ga, uint16_t gpios);
 
 #endif
--- Lib/Src/gpio_array.c
+++ Lib/Src/gpio_array.c
@@ -11,3 +11,17 @@ void gpio_array_init(gpio_array_t *ga, GPIO_TypeDef *port, uint16_t *pins,
   for (size_t i = 0; i < ga->len; i++)
     ga->mask |= ga->pins[i];
 }
+
+void gpio_array_set(gpio_array_t *ga, uint16_t gpios) {
+  HAL_GPIO_WritePin(ga->port, gpios, GPIO_PIN_SET);
+}
+
+void gpio_array_reset(gpio_array_t *ga, uint16_t gpios) {
+  HAL_GPIO_WritePin(ga->port, gpios, GPIO_PIN_RESET);
+}
+
+void gpio_array_write(gpio_array_t *ga, uint16_t gpios) {
+  gpio_array_reset(ga, gpios);
+  gpio_array_set(ga, ga->mask & ~gpios);
+}
+
