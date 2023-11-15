9236ec59eb0db8fc35cc43b3f94528d0677f6b23
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-11-15 22:02:38 +0700

    segment_display: update

--- Lib/Inc/segment_display.h
+++ Lib/Inc/segment_display.h
@@ -4,42 +4,15 @@
 #include "gpio_array.h"
 #include "stm32f1xx_hal.h" // IWYU pragma: keep
 #include <stdbool.h>
-#include <string.h>
 
 typedef struct {
   gpio_array_t gpios;
   uint16_t digit_to_gpios[10];
-} segment_display_t;
-
-typedef struct {
-  segment_display_t sd;
-  gpio_array_t en_gpios;
   size_t pos;
 } segment_display_array_t;
 
-void segment_display_init(segment_display_t *sd, GPIO_TypeDef *port,
-                          uint16_t *pins) {
-  gpio_array_init(&sd->gpios, port, pins, 7);
-  memset(sd->digit_to_gpios, 0, 20);
-
-  static bool segment_states[10][7] = {
-      {1, 1, 1, 1, 1, 1, 0}, /* */
-      {0, 1, 1, 0, 0, 0, 0}, /* */
-      {1, 1, 0, 1, 1, 0, 1}, /* */
-      {1, 1, 1, 1, 0, 0, 1}, /* */
-      {0, 1, 1, 0, 0, 1, 1}, /* */
-      {1, 0, 1, 1, 0, 1, 1}, /* */
-      {1, 0, 1, 1, 1, 1, 1}, /* */
-      {1, 1, 1, 0, 0, 0, 0}, /* */
-      {1, 1, 1, 1, 1, 1, 1}, /* */
-      {1, 1, 1, 1, 0, 1, 1}, /* */
-  };
-  for (uint8_t num = 0; num < 10; num++)
-    for (uint8_t i = 0; i < 7; i++)
-      if (segment_states[num][i])
-        sd->digit_to_gpios[num] |= sd->gpios.pins[i];
-}
-void segment_display_show(segment_display_t *sd, uint8_t num);
+void segment_display_array_init(segment_display_array_t *sda,
+                                GPIO_TypeDef *port, uint16_t *pins, size_t len);
 void segment_display_array_tick(segment_display_array_t *sda);
 void segment_display_array_show(segment_display_array_t *sd_arr,
                                 uint8_t *digits);
--- Lib/Src/segment_display.c
+++ Lib/Src/segment_display.c
@@ -1,17 +1,41 @@
 #include "segment_display.h"
+#include "gpio_array.h"
 #include <stdbool.h>
+#include <string.h>
 
-void segment_display_show(segment_display_t *sd, uint8_t num) {
-  gpio_array_write(&sd->gpios, sd->digit_to_gpios[num]);
+void segment_display_array_init(segment_display_array_t *sda,
+                                GPIO_TypeDef *port, uint16_t *pins,
+                                size_t len) {
+  gpio_array_init(&sda->gpios, port, pins, len);
+  memset(sda->digit_to_gpios, 0, 20);
+  sda->pos = 7;
+
+  static bool segment_states[10][7] = {
+      {1, 1, 1, 1, 1, 1, 0}, /* */
+      {0, 1, 1, 0, 0, 0, 0}, /* */
+      {1, 1, 0, 1, 1, 0, 1}, /* */
+      {1, 1, 1, 1, 0, 0, 1}, /* */
+      {0, 1, 1, 0, 0, 1, 1}, /* */
+      {1, 0, 1, 1, 0, 1, 1}, /* */
+      {1, 0, 1, 1, 1, 1, 1}, /* */
+      {1, 1, 1, 0, 0, 0, 0}, /* */
+      {1, 1, 1, 1, 1, 1, 1}, /* */
+      {1, 1, 1, 1, 0, 1, 1}, /* */
+  };
+  for (uint8_t num = 0; num < 10; num++)
+    for (uint8_t i = 0; i < 7; i++)
+      if (segment_states[num][i])
+        sda->digit_to_gpios[num] |= sda->gpios.pins[i];
 }
 
 void segment_display_array_tick(segment_display_array_t *sda) {
   sda->pos += 1;
-  sda->pos %= sda->en_gpios.len;
+  if (sda->pos == sda->gpios.len)
+    sda->pos = 7;
 }
 
 void segment_display_array_show(segment_display_array_t *sda, uint8_t *digits) {
-  gpio_array_set(&sda->en_gpios, sda->en_gpios.mask);
-  segment_display_show(&sda->sd, digits[sda->pos]);
-  gpio_array_reset(&sda->en_gpios, sda->en_gpios.pins[sda->pos]);
+  uint16_t write_pins = sda->digit_to_gpios[digits[sda->pos - 7]];
+  write_pins |= sda->gpios.pins[sda->pos];
+  gpio_array_write(&sda->gpios, write_pins);
 }
